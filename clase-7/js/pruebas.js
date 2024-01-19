61070511beaeff095ae39ccb133f0675f52daf7f
plizaso <58313867+plizaso@users.noreply.github.com>
2024-01-19 20:30:06 -0300

    Se termina el ejercicio del nivel-1

--- clase-7/js/pruebas.js
+++ clase-7/js/pruebas.js
@@ -1,14 +1,20 @@
 function probarValidarNombre() {
     console.assert(
-        validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+        validarNombre('') === 'El campo nombre debe tener al menos 1 caracter',
         'validarNombre no valido que el nombre no sea vacio',
     );
 
     const CANTIDAD_MAXIMA_CARACTERES = 20;
     console.assert(
-        validarNombre('111111111111111111111') ===
-        `Este campo debe tener menos de ${CANTIDAD_MAXIMA_CARACTERES} caracteres`,
+        validarNombre('aaaaaaaaaaaaaaaaaaaaa') ===
+        `El campo nombre debe tener menos de ${CANTIDAD_MAXIMA_CARACTERES} caracteres`,
         `validarNombre falla cuando el nombre tiene mas de ${CANTIDAD_MAXIMA_CARACTERES} caracteres`,
+    );
+
+    console.assert(
+        validarNombre('11111') ===
+        'El campo nombre debe tener solo letras',
+        'validarNombre falla con un nombre que no tiene solo letras'
     );
 
     console.assert(
@@ -26,20 +32,24 @@ function probarValidarCiudad() {
 
 function probarValidarDescripcionRegalo() {
     console.assert(
-        validarDescripcionRegalo('') === 'Este campo no debe estar vacio',
+        validarDescripcionRegalo('') === 'El campo descripcion no debe estar vacio',
         'validarDescripcionRegalo falla cuando la descripcion es vacia'
     );
 
     const CANTIDAD_MAXIMA_CARACTERES = 50;
     console.assert(
         validarDescripcionRegalo('111111111111111111111111111111111111111111111111111') ===
-        `Este campo debe tener menos de ${CANTIDAD_MAXIMA_CARACTERES} caracteres`,
+        `El campo descripcion debe tener menos de ${CANTIDAD_MAXIMA_CARACTERES} caracteres`,
         `validarDescripcionRegalo falla cuando la descripcion tiene mas de ${CANTIDAD_MAXIMA_CARACTERES} caracteres`
     );
 
     console.assert(
         validarDescripcionRegalo('Una lista de texto valida') === '',
         `validarDescripcionRegalo falla con una descripcion valida`
+    );
+    console.assert(
+        validarDescripcionRegalo('abc.,@_') === 'El campo descripcion debe tener solo letras y numeros',
+        'validarDescripcionRegalo falla cuando la descripcion tiene caracteres invalidos'
     );
 }
 
