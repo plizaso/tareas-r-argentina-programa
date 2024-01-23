7c2929ecb627b9561ebbf5bb9376a29832005c43
plizaso <58313867+plizaso@users.noreply.github.com>
2024-01-22 21:25:03 -0300

    resolucion tareas clase 8
    Se agregan validaciones y pruebas en las tareas 1 y 2 de la clase 6

--- clase-8/tarea-1-clase-6/js/tarea-1.js
+++ clase-8/tarea-1-clase-6/js/tarea-1.js
@@ -1,13 +1,52 @@
 document.querySelector('#btn-continuar').onclick = function () {
-    let cantidadIntegrantes = Number(document.querySelector('#cantidad-integrantes').value);
-    if (cantidadIntegrantes <= 0) {
-        alert('La cantidad de integrantes debe ser un número positivo.');
-        return false;
+    const idCantidadIntegrantes = '#cantidad-integrantes';
+    const cantidadIntegrantes = document.querySelector(idCantidadIntegrantes).value;
+    const errorIntegrantes = validarCantidadIntegrantes(cantidadIntegrantes);
+    if(!errorIntegrantes){
+        ocultarError(idCantidadIntegrantes, errorIntegrantes);
+        crearIntegrantes(Number(cantidadIntegrantes));
+        deshabilitarBotonContinuar();
+    }else{
+        mostrarError(idCantidadIntegrantes, errorIntegrantes);
     }
-    crearIntegrantes(cantidadIntegrantes);
-    mostrarIntegrantes();
-    deshabilitarBotonContinuar()
     return false;
+}
+
+function mostrarError(id, error){
+    let $input = document.querySelector(id);
+    $input.className = 'error';
+    $input.nextElementSibling.textContent = `*${error}`;
+}
+
+function ocultarError(id){
+    let $input = document.querySelector(id);
+    $input.className = '';
+    $input.nextElementSibling.textContent = '';
+}
+
+function validarNumero(numero){
+    if(numero===''){
+        return 'El campo no debe estar vacio';
+    }
+    numero = Number(numero);
+    if(isNaN(numero)){
+        return 'El valor ingresado debe ser un numero';
+    }
+    if(numero<=0){
+        return 'El numero debe ser mayor a 0';
+    }
+    if(!Number.isInteger(numero)){
+        return 'El numero debe ser un entero'
+    } 
+    return '';
+}
+
+function validarCantidadIntegrantes(cantidadIntegrantes){
+    return validarNumero(cantidadIntegrantes);
+}
+
+function validarEdad(edad){
+    return validarNumero(edad);
 }
 
 function crearIntegrantes(cantidadIntegrantes) {
@@ -15,12 +54,14 @@ function crearIntegrantes(cantidadIntegrantes) {
     for (let i = cantidadIntegrantes; i > 0; i--) {
         $integrantes.prepend(crearIntegrante(i));
     }
+    mostrarIntegrantes();
 }
 
 function crearIntegrante(numero) {
-    let $label = crearLabel(numero);
-    $label.appendChild(crearInput(numero));
-    return $label;
+    let $integrante = crearLabel(numero);
+    $integrante.appendChild(crearInput(numero));
+    $integrante.appendChild(crearError());
+    return $integrante;
 }
 
 function crearLabel(numero) {
@@ -38,39 +79,61 @@ function crearInput(numero) {
     return $input;
 }
 
+function crearError(){
+    let $error = document.createElement('span');
+    $error.className = 'error-text';
+    return $error;
+}
+
 document.querySelector('#btn-calcular').onclick = function () {
     let edades = obtenerEdades();
-    if (validarEdades(edades)) {
-        mostrarEdades(edades);
-        mostrarResultados();
-    } else {
-        alert('Las edades deben ser mayores a 0.');
+    const errores = validarEdades(edades);
+    const cantidadErrores = manejarErroresEdad(errores);
+    if(cantidadErrores === 0){
+        mostrarEdades(edades.map((edad) => Number(edad)));
+    }else{
+        borrarResultados();
     }
     return false;
 }
 
-function obtenerEdades() {
-    let $integrantes = document.querySelectorAll('#integrantes input');
-    let edades = [];
-    for (let i = 0; i < $integrantes.length; i++) {
-        edades.push(Number($integrantes[i].value));
+function manejarErroresEdad(errores){
+    let cantidadErrores = 0;
+    for (let i = 0; i < errores.length; i++) {
+        const error = errores[i];
+        const idEdadIntegrante = `#edad-integrante-${i+1}`;
+        if(error){
+            cantidadErrores++;
+            mostrarError(idEdadIntegrante, error);
+        }else{
+            ocultarError(idEdadIntegrante);
+        }
     }
+    return  cantidadErrores;
+}
+
+function obtenerEdades() {
+    let $edadesIntegrantes = document.querySelectorAll('#integrantes input');
+    let edades = [];
+    $edadesIntegrantes.forEach($edad => {
+        edades.push($edad.value);
+    });
     return edades;
 }
 
 function validarEdades(edades) {
-    for (let i = 0; i < edades.length; i++) {
-        if (edades[i] <= 0) {
-            return false;
-        }
-    }
-    return true;
+    const errores = [];
+    edades.forEach(edad => {
+        errores.push(validarEdad(edad));
+    });
+    return errores;
 }
 
 function mostrarEdades(edades) {
     mostrarEdad('#mayor-edad', obtenerMaximo(edades));
     mostrarEdad('#menor-edad', obtenerMinimo(edades));
     mostrarEdad('#edad-promedio', obtenerPromedio(edades));
+    mostrarResultados();
 }
 
 function mostrarEdad(id, edad) {
@@ -142,11 +205,11 @@ function ocultarResultados() {
 }
 
 function mostrarElemento(id){
-    document.querySelector(id).setAttribute('style', 'display: block');
+    document.querySelector(id).className = '';
 }
 
 function ocultarElemento(id){
-    document.querySelector(id).setAttribute('style', 'display: none');
+    document.querySelector(id).className = 'oculto';
 }
 
 function deshabilitarBotonContinuar() {
